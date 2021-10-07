382573c9bf184a480b05f9c14d571b99767a9f27
Tim Schäfer <ts+code@rcmd.org>
2021-10-07 22:01:00 +0200

    better description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='lgilearn',
     version='0.0.1',
-    description='Predict local gyrification index for mesh vertex',
+    description='Predict the local gyrification index for a mesh vertex. The local gyrification index is a brain morphometry descriptor used in computational neuroimaging. It describes the folding of the human cortex at a specific point, based on a mesh reconstruction of the cortical surface from a magnetic resonance image.',
     url='https://github.com/dfsp-spirit/lgilearn',
     author='Tim Schaefer',
     author_email='ts+code@rcmd.org',

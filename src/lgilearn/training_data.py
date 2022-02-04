13b89b615f5928100a9dd16dfd28b58e39fd3667
Tim Schäfer <ts+code@rcmd.org>
2022-02-04 15:17:43 +0100

    finish functions for loading LGI and mesh data

--- src/lgilearn/training_data.py
+++ src/lgilearn/training_data.py
@@ -1,20 +1,19 @@
 """
-Read FreeSurfer brain meshes and computed lgi per-vertex data for them from a directory.
+Read FreeSurfer brain meshes and pre-computed lgi per-vertex data for them from a directory.
 """
 
-import os
-import numpy as np
-import nibabel.freesurfer.io as fsio
-#from . import nitools as nit
-#from . import freesurferdata as fsd
-import logging
 import brainload as bl
 
 
 def load_piallgi_morph_data(subjects_dir, subjects_list):
     return bl.group_native("pial_lgi", subjects_dir, subjects_list)
 
-def load_surfaces(subjects_dir, subjects_list):
-    surface = "pial"
+
+def load_surfaces(subjects_dir, subjects_list, surf="pial"):
+    meshes = {}
     for subject in subjects_list:
+        verts, faces, meta_data = bl.subject_mesh(subject, subjects_dir, surf=surf)
+        meshes[subject] = { "vertices": verts, "faces" : faces }
+    return meshes
+        
         

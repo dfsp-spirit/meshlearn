00013d61158eaf0947e8bb85769a6ad3a71d076a
Tim Schäfer <ts+code@rcmd.org>
2022-02-22 23:10:58 +0100

    add -n

--- tests/do_deepcopy.sh
+++ tests/do_deepcopy.sh
@@ -4,7 +4,7 @@ SOURCE_DIR="$1"
 DEST_DIR="$2"
 
 if [ -z "${SOURCE_DIR}" -o -z "${DEST_DIR}" ]; then
-    echo "SYNTAX ERROR ;)"
+    echo "ERROR: SYNTAX ERROR ;)"
     echo "USAGE: $0 <source_dir> <dest_dir>"
     exit 1
 fi
@@ -18,7 +18,7 @@ if [ ! -d "${DEST_DIR}" ]; then
 fi
 
 if [ ! -f "./deepcopy_testdata.py" ]; then
-    echo "Please run this from the <repo_root>/tests/ directory."
+    echo "Please run this script from the '<repo_root>/tests/' directory."
 fi
 
-python ./deepcopy_testdata.py -s "${SOURCE_DIR}" -t "{$DEST_DIR}" -f ./deepcopy_filelist.txt
+python ./deepcopy_testdata.py -s "${SOURCE_DIR}" -t "{$DEST_DIR}" -f ./deepcopy_filelist.txt -n

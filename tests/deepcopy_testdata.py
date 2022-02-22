f5fd7a673afa61def7cac4bb4c861ce7c4f8bc64
Tim Schäfer <ts+code@rcmd.org>
2022-02-22 14:57:52 +0100

    implement not-so-deep mode

--- tests/deepcopy_testdata.py
+++ tests/deepcopy_testdata.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 import sys
 import os
 import argparse
+
+from matplotlib.cbook import safe_masked_invalid
 import brainload.nitools as nit
 import shutil
 
@@ -22,6 +24,8 @@ def deepcopy_testdata_freesurfer():
     file_group.add_argument('-f', '--file', nargs=1, help="The file to copy, relative to <source_dir>/<subject>/.", default="surf/lh.pial")
     parser.add_argument('-l', '--subjects-file', nargs=1, help="The subjects file to use. Text file with one subject per line. If left at default, <source_dir>/subjects.txt is used.", default="_")
     parser.add_argument('-d', '--descriptor', nargs=1, help="The descriptor to copy.", default="pial_lgi")
+    parser.add_argument("-n", "--not-so-deep", help="Do not deep-copy, rename files to reflect origin and place all directly in target-dir instead.", action="store_true")
+    parser.add_argument('-a', '--add-suffix', nargs=1, help="An optional suffix to add to the target file name.", default="")
     args = parser.parse_args()
 
 
@@ -52,17 +56,41 @@ def deepcopy_testdata_freesurfer():
     else:
         files_per_subject = nit.read_subjects_file(args.file_list)    # It is not really a subjects file, but the format is the same.
 
+    num_files_total = len(files_per_subject)*len(subjects_list)
+    if args.verbose:
+        print("Copying {num_files_per_subject} files per subject for {num_subjects} subjects: {num_files_total} files in total.".format(num_files_per_subject=len(files_per_subject), num_subjects=len(subjects_list), num_files_total=num_files_total))
+
+    if args.verbose:
+        if args.not_so_deep:
+            print("Using not-so-deep mode: copying all files directly into directory {target_dir} with adapted file names.".format(target_dir=args.target_dir))
+        else:
+            print("Using deep mode: copying all files into recon-all directory structure under {target_dir}.".format(target_dir=args.target_dir))
+
     num_files_failed = 0
     for subject in subjects_list:
         for sfile_rel in files_per_subject:
             source_file = os.path.join(args.source_dir, subject, sfile_rel)
             subject_rel_dir = os.path.dirname(os.path.join(subject, sfile_rel)) # required below for reconstruction/creation of target path.
-            dest_file = os.path.join(args.target_dir, subject, sfile_rel)
+
+            if args.not_so_deep:
+                sfile_rel_no_dir_separator = sfile_rel.replace(os.sep, "_") # replace the OS-specific dir separator with an underscore.
+                target_file_name = "{subject}_{sfile}".format(subject=subject, sfile=sfile_rel_no_dir_separator)
+                dest_file = os.path.join(args.target_dir, target_file_name)
+                dest_subdir = args.target_dir
+            else:
+                dest_file = os.path.join(args.target_dir, subject, sfile_rel)
+                dest_subdir = os.path.join(args.target_dir, subject_rel_dir)
+
+            if args.add_suffix:
+                dest_file = "{dest_file}{suffix}".format(dest_file=dest_file,suffix=args.suffix)
+                if args.verbose:
+                    print("Added suffix {suffix}, dest file is now '{dest_file}'".format(suffix=args.suffix, dest_file=dest_file))
+
             if not os.path.isfile(source_file):
                 print("WARNING: Expected source file '{source_file}' does not exist or cannot be read, skipping.".format(source_file=source_file))
                 num_files_failed = num_files_failed + 1
             else:
-                dest_subdir = os.path.join(args.target_dir, subject_rel_dir)
+                
                 if not os.path.exists(dest_subdir):
                     os.makedirs(dest_subdir, exist_ok=True)
                 try:
@@ -70,8 +98,8 @@ def deepcopy_testdata_freesurfer():
                 except:
                     print("WARNING: Failed to copy source file '{source_file}' to destination '{dest_file}'.".format(source_file=source_file, dest_file=dest_file))
                     num_files_failed = num_files_failed + 1
-    num_total = len(files_per_subject)*len(subjects_list)
-    print("Done. {num_failed} of {num_total} files failed ({num_ok} okay).".format(num_failed=num_files_failed, num_total=num_total, num_ok=num_total-num_files_failed))
+    
+    print("Done. {num_failed} of {num_total} files failed ({num_ok} okay).".format(num_failed=num_files_failed, num_total=num_files_total, num_ok=num_files_total-num_files_failed))
 
 
 if __name__ == "__main__":

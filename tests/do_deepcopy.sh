e09eee5802fe1cd304e8c4e2bfb94f50c6fe5296
Tim Schäfer <ts+code@rcmd.org>
2022-02-22 21:35:15 +0100

    add deepcopy script

---
+++ tests/do_deepcopy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SOURCE_DIR="$1"
+DEST_DIR="$2"
+
+if [ -z "${SOURCE_DIR}" -o -z "${DEST_DIR}" ]; then
+    echo "SYNTAX ERROR ;)"
+    echo "USAGE: $0 <source_dir> <dest_dir>"
+    exit 1
+fi
+
+if [ ! -f "./deepcopy_testdata.py" ]; then
+    echo "Please run this from the <repo_root>/tests/ directory."
+fi
+
+python ./deepcopy_testdata.py -s "${SOURCE_DIR}" -t "{$DEST_DIR}" -f ./deepcopy_filelist.txt

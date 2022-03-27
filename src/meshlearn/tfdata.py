fix neighborhood computation for k > 1

src/meshlearn/tfdata.py
@@ -63,17 +63,29 @@ class VertexPropertyDataset(tf.data.Dataset):
 # parameter tmesh must be a mesh instance from the trimesh package
 def _k_neighborhoods(tmesh, k=1):
     neighborhoods = dict()
-    print("Mesh has {nv} vertices, ccords are in {d}d space.".format(nv=tmesh.vertices.shape[0], d=tmesh.vertices.shape[1]))
+    print("Mesh has {nv} vertices, coords are in {d}d space.".format(nv=tmesh.vertices.shape[0], d=tmesh.vertices.shape[1]))
     for vert_idx in range(tmesh.vertices.shape[0]):
         neighborhoods[vert_idx] = np.array(tmesh.vertex_neighbors[vert_idx])
     if k == 1:
         return neighborhoods
     else:
-        for step_idx in range(0, k):
-            for vert_idx in keys(neighborhoods):
+        for step_idx in range(1, k):
+            print("Computing k-neighborhoods for k={step_idx}, will compute up to k={k}.".format(step_idx=step_idx, k=k))
+            for vert_idx in neighborhoods.keys():
                 cur_neighbors = neighborhoods[vert_idx]
-                neighborhoods[vert_idx] = np.unique([neighborhoods.get(key) for key in cur_neighbors])
+                neighborhoods[vert_idx] = np.unique(np.concatenate([neighborhoods.get(key) for key in cur_neighbors]))
+    nsizes = [len(v) for k,v in neighborhoods.items()]
     return neighborhoods
+
+
+# Extract vertex coords of all neighborhood vertices and center them, so that
+# the respective source vertex is at the origin.
+def _neighborhoods_centered_coords(neighborhoods, tmesh, num_neighbors=10):
+    neigh_coords = np.ndarray(shape=(num_neighbors, 3), dtype=float)
+    vert_idx = 0
+    for central_vertex, neighbors in neighborhoods.items():
+        central_coords = tmesh.vertices[central_vertex, :]
+        neigh_coords[vert_idx, :] = tmesh.vertices[central_vertex]
 
 
 
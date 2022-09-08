45fdd0dab62a3687f4cf4de779aa71db1f078376
Tim Schäfer <ts+code@rcmd.org>
2022-09-08 11:16:24 +0200

    WIP: work on tf model

--- src/meshlearn/clients/meshlearn_tf_dnn.py
+++ src/meshlearn/clients/meshlearn_tf_dnn.py
@@ -11,17 +11,19 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import Sequential
 from tensorflow.keras import optimizers
-
+from sklearn.preprocessing import StandardScaler
 
 ##### Settings #####
 
 #data_dir = os.path.expanduser("~/develop/cpp_geodesics/output/")
 data_dir = os.path.expanduser("/media/spirit/science/data/abide_meshlearn/")
 
-max_files_to_load = 8  # Set to something <= 0 for unlimited.
+max_files_to_load = 2  # Set to something <= 0 for unlimited.
 
 ##### End of settings #####
 
+
+#### Functions #####
 
 def load_data(data_dir, max_files_to_load):
     csv_files = glob.glob(os.path.join(data_dir, "*meshdist_edge_5_neigh.csv"))
@@ -77,16 +79,83 @@ def load_data(data_dir, max_files_to_load):
     print(f"Loaded dataset with shape {dataset.shape} from {num_files_loaded} files. Ignored {num_incompatible} CSV files with incompatible column count.")
     return dataset
 
+
+######################################## Start script ########################################
+
+
+##### Load data #####
+
 dataset = load_data(data_dir, max_files_to_load)
 
 dataset_num_rows_with_na = dataset.isna().any(axis=0).sum()
 dataset_num_cols_with_na = dataset.isna().any(axis=1).sum()
 
 if dataset_num_rows_with_na > 0:
-    print(f"WARNING: Found {dataset_num_rows_with_na} rows with NA values in dataset (and {dataset_num_cols_with_na} columns).")
+    print(f"Found {dataset_num_rows_with_na} rows with NA values in dataset (and {dataset_num_cols_with_na} columns). Dropping them.")
+    dataset.dropna(axis=0, inplace=True)
 else:
     print(f"Found no NA values in dataset.")
 
 
+##### Split into train and test data #####
+
+
+train_dataset = dataset.sample(frac=0.8, random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+
+# Separate labels and features
+label_column_name = "label"
+
+train_features = train_dataset.drop([label_column_name], axis=1)
+test_features = test_dataset.drop([label_column_name], axis=1)
+train_labels = train_dataset[label_column_name]
+test_labels = test_dataset[label_column_name]
+
+
+##### Feature scaling #####
+
+feature_scaler = StandardScaler()
+label_scaler = StandardScaler()
+
+##  Fit on Training Data
+feature_scaler.fit(train_features.values)
+label_scaler.fit(train_labels.values.reshape(-1, 1))
+
+## Transform both training and testing data
+train_features = feature_scaler.transform(train_features.values)
+test_features = feature_scaler.transform(test_features.values)
+train_labels = label_scaler.transform(train_labels.values.reshape(-1, 1))
+test_labels = label_scaler.transform(test_labels.values.reshape(-1, 1))
+
+
+##### Define model #####
+
+# Now let's create a Deep Neural Network to train a regression model on our data.
+model = Sequential([
+    layers.Dense(32, activation='relu'),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(1)
+])
+
+model.compile(optimizer="RMSProp",
+              loss="mean_squared_error")
+
+##### Fit model #####
+
+history = model.fit(epochs=100, x=train_features, y=train_labels,
+          validation_data=(test_features, test_labels), verbose=1)
+
+##### Analyze training #####
+
+def plot_loss(history):
+    plt.plot(history.history['loss'], label='loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
+    plt.ylim([0,10])
+    plt.xlabel('Epoch')
+    plt.ylabel('Error (Loss)')
+    plt.legend()
+    plt.grid(True)
+
+plot_loss(history)
 
 

62b4f9b2fcc5dc1782f5474f091841c7da3073a7
Tim Schaefer <ts+code@rcmd.org>
2022-09-16 09:24:34 +0200

    CHG: do not check for test data dir in run script

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-abide_lgi_dir="tests/test_data/abide"
-if [ ! -d "${abide_lgi_dir}" ]; then
-    echo "ERROR: Cannot find the ABIDE lGI data at '${abide_lgi_dir}'."
-    echo "  Please run the 'tests/do_deepcopy.sh' on your recon-all"
-    echo "  ABIDE output directory after computing pial-lgi in FreeSurfer."
-    exit 1
-fi
+#abide_lgi_dir="tests/test_data/abide"
+#if [ ! -d "${abide_lgi_dir}" ]; then
+#    echo "ERROR: Cannot find the ABIDE lGI data at '${abide_lgi_dir}'."
+#    echo "  Please run the 'tests/do_deepcopy.sh' on your recon-all"
+#    echo "  ABIDE output directory after computing pial-lgi in FreeSurfer."
+#    exit 1
+#fi
 
 python ./src/meshlearn/clients/meshlearn_lgi.py -v

b3d830176a196417ad591301de7d791a94daa508
Tim Schaefer <ts+code@rcmd.org>
2022-09-27 10:55:49 +0200

    CHG: remove unused functions

--- src/meshlearn/training_data.py
+++ src/meshlearn/training_data.py
@@ -21,18 +21,6 @@ import json
 from datetime import timedelta
 from sys import getsizeof
 
-def load_piallgi_morph_data(subjects_dir, subjects_list):
-    return bl.group_native("pial_lgi", subjects_dir, subjects_list)
-
-
-def load_surfaces(subjects_dir, subjects_list, surf="pial"):
-    meshes = {}
-    for subject in subjects_list:
-        vertices, faces, meta_data = bl.subject_mesh(subject, subjects_dir, surf=surf)
-        #meshes[subject] = { "vertices": vertices, "faces" : faces }
-        meshes[subject] = tm.Trimesh(vertices=vertices, faces=faces)
-    return meshes
-
 
 class TrainingData():
 

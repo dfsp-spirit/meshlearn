5f4eb15a1fc55669446f452a0b3acc7cffbbb0a5
Tim Schäfer <ts+code@rcmd.org>
2022-10-16 16:06:30 +0200

    CHG: remove tensorflow dependency, cleanups

--- setup.py
+++ setup.py
@@ -3,23 +3,22 @@ from setuptools import setup
 setup(
     name='meshlearn',
     version='0.0.1',
-    description='Predict vertex-wise mesh descriptors. E.g., predict the local gyrification index for a mesh vertex. The local gyrification index is a brain morphometry descriptor used in computational neuroimaging. It describes the folding of the human cortex at a specific point, based on a mesh reconstruction of the cortical surface from a magnetic resonance image.',
+    description='Predict local vertex-wise mesh descriptors. E.g., predict the local gyrification index for a mesh vertex. The local gyrification index is a brain morphometry descriptor used in computational neuroimaging. It describes the folding of the human cortex at a specific point, based on a mesh reconstruction of the cortical surface from a magnetic resonance image.',
     url='https://github.com/dfsp-spirit/meshlearn',
     author='Tim Schaefer',
     author_email='ts+code@rcmd.org',
     license='MIT',
     packages=['meshlearn'],
-    install_requires=['tensorflow>=2.0',
-                      'tensorflow_datasets',
-                      'numpy',
+    install_requires=['numpy',
                       'matplotlib',
+                      'pandas',
                       'nibabel',
                       'trimesh',
                       'brainload',
                       'sklearn'
                       ],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'pytest-cov', 'pytest-console-scripts', 'pytest-runner', 'tox'],
+    tests_require=['pytest', 'pytest-cov', 'pytest-console-scripts', 'pytest-runner', 'coverage'],
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Science/Research',
@@ -30,7 +29,7 @@ setup(
     package_dir = {'': 'src'},
     entry_points = {
         'console_scripts': [
-            'meshlearn_lgi = meshlearn.clients.meshlearn_lgi:main',
+            'meshlearn_lgi = clients.meshlearn_lgi:main',
         ]
     }
 )

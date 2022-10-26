d80f2a736167ac9c9507238db2b7f83c17983c89
Tim Schäfer <ts+code@rcmd.org>
2022-10-26 11:50:55 +0200

    FIX: use integers in int part of test

--- tests/meshlearn/test_mem_opt.py
+++ tests/meshlearn/test_mem_opt.py
@@ -10,8 +10,8 @@ import numpy as np
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 TEST_DATA_DIR = os.path.join(THIS_DIR, os.pardir, 'test_data')
-# Respect the environment variable BRAINLOAD_TEST_DATA_DIR if it is set. If not, fall back to default:
-TEST_DATA_DIR = os.getenv('BRAINLOAD_TEST_DATA_DIR', TEST_DATA_DIR)
+# Respect the environment variable MESHLEARN_TEST_DATA_DIR if it is set. If not, fall back to default:
+TEST_DATA_DIR = os.getenv('MESHLEARN_TEST_DATA_DIR', TEST_DATA_DIR)
 
 
 def test_mem_opt_float16_range():
@@ -35,8 +35,8 @@ def test_mem_opt_float():
 
 def test_mem_opt_int():
     for dtype in [np.int8, np.int16, np.int32, np.int64]:
-        low = np.iinfo(dtype).max - 100
-        data = np.random.default_rng().uniform(low=low, high=low+50, size=10)
+        low = np.iinfo(dtype).max - 50
+        data = np.random.default_rng().integers(low=low, high=low+30, size=10)
         df = pd.DataFrame({"data": data})
         df_opt = reduce_mem_usage(df)
         assert df.shape == df_opt.shape
